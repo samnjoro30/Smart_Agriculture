67b5155289636581842a6f0bc7a00fa7ede60ae3
Sam njoro <samnjorokibandi@gmail.com>
2025-06-23 13:36:01 +0300

    nginx setup

--- backend/API.py
+++ backend/API.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI 
-
+from fastapi.middleware.cores import CORSMiddleware
+from backend.db.database import Base, engine, get_db
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        '*'
+    ]
+    allow_methods = [
+        "*"
+    ]
+    allow_headers = [
+        "*"
+    ]
+)
+
+app.include_router()
 
 @app.get("/login")
 def login():
