a9121f9a9c727b55bad69a6bf5de9bc30f40f1d4
Sam njoro <samnjorokibandi@gmail.com>
2025-06-25 23:34:35 +0300

    complete setup

--- frontend/src/app/components/login.tsx
+++ frontend/src/app/components/login.tsx
@@ -1,9 +1,34 @@
-import { useState } from 'react';
+"use client"
+import React, { useState } from 'react';
+import axiosInstance from '../API/axiosInstance';
+import Cookies from 'js-cookie';
+import {useRouter} from 'next/navigation';
+import { parseSetCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
-export default function login() {
+export default function Login() {
    const [username, setUsername] = useState('')
    const [password, setPassword] = useState('')
-   const [error, setError] = useState('')
+   const [loading, setLoading] = useState(false);
+   const [error, setError] = useState('');
+   const Router = useRouter();
+
+   const [formData, setFormData] = useState({ username: '', password: '' });
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      setError('');
+      try {
+         const res = await axiosInstance.post('/login',{
+            formData
+         });
+         Cookies.set('token', res.data.token);
+         Router.push('/dashboard')
+      }catch(err){
+          console.error("Error trying to login", err);
+      }
+   }
+   
    return(
     <div>
        <h1>login form</h1>
@@ -11,8 +36,9 @@ export default function login() {
           <div>
             <label>Username:</label>
             <input 
+               id="username"
                name="name" 
-               type="name" 
+               type="text" 
                placeholder="sam njoro" 
                required>
               
@@ -21,6 +47,7 @@ export default function login() {
           <div>
             <label>password:</label>
             <input 
+               id="password"
                name="password"  
                type="password" 
                required 
