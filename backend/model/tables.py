ab2ec0aa3a4229bf54747bf62e63990ceeb58c6f
Sam njoro <samnjorokibandi@gmail.com>
2025-07-24 18:44:07 +0300

    refresh token db storage

--- backend/model/tables.py
+++ backend/model/tables.py
@@ -10,4 +10,14 @@ class Users(Base):
     farmname = Column(String(200))
     phonenumber = Column(String(200), unique=True)
     password = Column(String(200))
-    confirmpassword = Column(String(200))
+
+class RefreshToken(Base):
+
+    __tablename__ = "refresh_token"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username= Column(String(200), nullable=False)
+    token = Column(String(500), nullable=False)
+    expires_at = Column(DateTime, nullable=False)
+    is_revoked = Column(Boolean, default=False)
+
