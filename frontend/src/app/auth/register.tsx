b3f88c77b772e38e7595869483a70901a5ba4ea0
Sam njoro <samnjorokibandi@gmail.com>
2025-07-25 18:16:06 +0300

    registration

--- frontend/src/app/auth/register.tsx
+++ frontend/src/app/auth/register.tsx
@@ -35,7 +35,6 @@ const Register = () => {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
         const { name, value } = e.target;
         setFormData(prev => ({ ...prev, [name]: value}));
-
     }
 
     const handleSubmit = async (e: { preventDefault: () => void; }) =>{
