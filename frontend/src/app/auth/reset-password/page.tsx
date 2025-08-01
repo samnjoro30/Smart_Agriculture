ed2ca3ec68f08b47e730a6b91174303246cf4824
Sam njoro <samnjorokibandi@gmail.com>
2025-08-01 18:15:26 +0300

    reset passsword page

--- frontend/src/app/auth/reset-password/page.tsx
+++ frontend/src/app/auth/reset-password/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from 'react';
-import axiosInstance from "../API/axiosInstance";
+import axiosInstance from "../../API/axiosInstance";
 import { useRouter } from 'next/navigation';
 
 interface FormData {
