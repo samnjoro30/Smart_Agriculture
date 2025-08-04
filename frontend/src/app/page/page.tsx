0f45eef35464a00c49ec5af90f4f4f9cdf1a074b
Sam njoro <samnjorokibandi@gmail.com>
2025-08-04 19:58:24 +0300

    home page  services styling

--- frontend/src/app/page/page.tsx
+++ frontend/src/app/page/page.tsx
@@ -8,6 +8,9 @@ export  default function Homepage() {
     const currentYear = new Date().getFullYear();
 
     const liStyling = "hover:text-green hover:underline transition duration-200 cursor-pointer";
+    const serviceDiv ="bg-white rounded-xl shadow-md p-6 text-center hover:shadow-lg transition-shadow duration-300";
+    const Hdiv = "text-xl font-semibold text-green-800 mb-2";
+    const pservice = "text-gray-700 text-sm";
 
     return(
         <div className="min-h-screen flex flex-col">
@@ -44,38 +47,41 @@ export  default function Homepage() {
                         <div className="relative z-20 flex flex-col items-center justify-center h-full px-4 text-center text-white space-y-6">
                             <p className="max-w-2xl text-lg md:text-xl">This is a platform designed to help farmers track and improve productiivity in there farms by simple clicks where you enjoy seamless advantages of AI, ML prediction and timely production</p>
 
-                            <p className="text-green-200 font-semibold text-lg"> Farmers First choice</p>
+                            <p className="text-green-200 font-semibold text-lg">Make Chaguo Smart for your Farm, Register with us </p>
                         </div>
                     </div>
                 </section>
-                <section className="py-12 px-4 bg-green-100 shadow-black">
-                    <h2 className="">services</h2>
-                    <div className="">
-                        <Image  src="/logo.png" alt="logo" width={80} height={40}/>
-                        <h3>Farm Inventory</h3>
-                        <p>You get your record well architectured and simplified for you to understand. The inventory helps you to plan for the farm management </p>
+                <section className="py-12 px-4 bg-green-100 ">
+                    <h2 className="text-3xl md:text-4xl font-bold text-center text-green-700 mb-12">services</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 max-w-6xl mx-auto">
+                        
+                        <div className={serviceDiv}>
+                            <Image  src="/logo.png" alt="logo" width={100} height={40} className="mx-auto mb-4"/>
+                            <h3 className="text-xl font-semibold text-green-800 mb-2">Farm Inventory</h3>
+                            <p className="text-gray-700 text-sm">You get your record well architectured and simplified for you to understand. The inventory helps you to plan for the farm management </p>
 
-                    </div>
-                    <div className="">
-                        <Image  src="/logo.png" alt="logo" width={80} height={40}/>
-                        <h3>AI-powered Insights on your farm</h3>
-                        <p>Help analyze upto 97% correct prediction of the outcome of your produce and recommendation on how on steps to take to have a higher yield outcome</p>
-                    </div>
-                    <div className="">
-                        <Image  src="/logo.png" alt="logo" width={80} height={40}/>
-                        <h3>Real-time Farm Analytics</h3>
-                        <p>You get real-time updates on  your farm management and where to put more effort for higher productivity</p>
-                    </div>
-                    <div className="">
-                        <Image  src="/logo.png" alt="logo" width={80} height={40}/>
-                        <h3>Smart Irrigation</h3>
-                        <p>The Platform is designed to help you as a farmer migrate  and get started with modern and emerging  Agricultural technology form irrifation to dairy farming and many more</p>
+                        </div>
+                        <div className={serviceDiv}>
+                            <Image  src="/logo.png" alt="logo" width={100} height={40}/>
+                            <h3 className={Hdiv}>AI-powered Insights on your farm</h3>
+                            <p className={pservice}>Help analyze upto 97% correct prediction of the outcome of your produce and recommendation on how on steps to take to have a higher yield outcome</p>
+                        </div>
+                        <div className={serviceDiv}>
+                            <Image  src="/logo.png" alt="logo" width={100} height={40}/>
+                            <h3 className={Hdiv}>Real-time Farm Analytics</h3>
+                            <p className={pservice}>You get real-time updates on  your farm management and where to put more effort for higher productivity</p>
+                        </div>
+                        <div className={serviceDiv}>
+                            <Image  src="/logo.png" alt="logo" width={100} height={40}/>
+                            <h3 className={Hdiv}>Smart Irrigation</h3>
+                            <p className={pservice}>The Platform is designed to help you as a farmer migrate  and get started with modern and emerging  Agricultural technology form irrifation to dairy farming and many more</p>
 
-                    </div>
-                    <div className="">
-                        <Image  src="/logo.png" alt="logo" width={80} height={40}/>
-                        <h3>Breeding Tracking</h3>
-                        <p>We are helping farmers track there breeding enabling upto 99% correct prediction of breeding interval, type of breed</p>
+                        </div>
+                        <div className={serviceDiv}>
+                            <Image  src="/logo.png" alt="logo" width={100} height={40}/>
+                            <h3 className={Hdiv}>Breeding Tracking</h3>
+                            <p className={pservice}>We are helping farmers track there breeding enabling upto 99% correct prediction of breeding interval, type of breed</p>
+                        </div>
                     </div>
                 </section>
                 <section>
