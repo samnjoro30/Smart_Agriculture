3b4dfaa1ac7d644858c9133759848e24005e9756
Sam njoro <samnjorokibandi@gmail.com>
2025-08-04 19:19:46 +0300

    hosting frontend url

--- backend/API.py
+++ backend/API.py
@@ -20,7 +20,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["http://localhost:3000", "https://smart-agriculture-git-main-samnjoro30s-projects.vercel.app/"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
