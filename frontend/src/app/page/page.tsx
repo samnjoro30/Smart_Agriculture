815c14d6d6ed840b9c2b3ce97aa27c86433939a8
Sam njoro <samnjorokibandi@gmail.com>
2025-08-05 17:42:43 +0300

    services section styling

--- frontend/src/app/page/page.tsx
+++ frontend/src/app/page/page.tsx
@@ -11,6 +11,7 @@ export  default function Homepage() {
     const serviceDiv ="bg-white rounded-xl shadow-md p-6 text-center hover:shadow-lg transition-shadow duration-300";
     const Hdiv = "text-xl font-semibold text-green-800 mb-2";
     const pservice = "text-gray-700 text-sm";
+    const ImageDiv = "w-80 h-50 relative mb-4 overflow-hidden rounded-md";
 
     return(
         <div className="min-h-screen flex flex-col">
@@ -51,37 +52,41 @@ export  default function Homepage() {
                         </div>
                     </div>
                 </section>
+
                 <section className="py-12 px-4 bg-green-100 ">
                     <h2 className="text-3xl md:text-4xl font-bold text-center text-green-700 mb-12">services</h2>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 max-w-6xl mx-auto">
-                        
                         <div className={serviceDiv}>
-                            <Image  src="/logo.png" alt="logo" width={100} height={40} className="mx-auto mb-4"/>
-                            <h3 className="text-xl font-semibold text-green-800 mb-2">Farm Inventory</h3>
-                            <p className="text-gray-700 text-sm">You get your record well architectured and simplified for you to understand. The inventory helps you to plan for the farm management </p>
-
+                            <Image  src="/agri_cow.jpg" alt="Inventory" width={100} height={40} className={ImageDiv}/>
+                            <h3 className={Hdiv}>Farm Inventory</h3>
+                            <p className={pservice}>You get your record well architectured and simplified for you to understand. The inventory helps you to plan for the farm management </p>
                         </div>
+
                         <div className={serviceDiv}>
-                            <Image  src="/logo.png" alt="logo" width={100} height={40}/>
+                            <Image  src="/Agri4.jpg" alt="AI image" width={100} height={40} className={ImageDiv} />
                             <h3 className={Hdiv}>AI-powered Insights on your farm</h3>
                             <p className={pservice}>Help analyze upto 97% correct prediction of the outcome of your produce and recommendation on how on steps to take to have a higher yield outcome</p>
+                            
                         </div>
+
                         <div className={serviceDiv}>
-                            <Image  src="/logo.png" alt="logo" width={100} height={40}/>
+                            <Image  src="/Agri5.png" alt="Real-analytics" width={100} height={40} className={ImageDiv}/>
                             <h3 className={Hdiv}>Real-time Farm Analytics</h3>
                             <p className={pservice}>You get real-time updates on  your farm management and where to put more effort for higher productivity</p>
                         </div>
+
                         <div className={serviceDiv}>
-                            <Image  src="/logo.png" alt="logo" width={100} height={40}/>
+                            <Image  src="/agri2.jpg" alt="Irrigation" width={100} height={40} className={ImageDiv}/>
                             <h3 className={Hdiv}>Smart Irrigation</h3>
                             <p className={pservice}>The Platform is designed to help you as a farmer migrate  and get started with modern and emerging  Agricultural technology form irrifation to dairy farming and many more</p>
+                        </div>
 
-                        </div>
                         <div className={serviceDiv}>
-                            <Image  src="/logo.png" alt="logo" width={100} height={40}/>
+                            <Image  src="/cow3.jpg" alt="breeding" width={100} height={40} className={ImageDiv} />
                             <h3 className={Hdiv}>Breeding Tracking</h3>
-                            <p className={pservice}>We are helping farmers track there breeding enabling upto 99% correct prediction of breeding interval, type of breed</p>
+                            <p className={pservice}>Track breeding cycles with up to 99% accuracy, ensuring optimal timing and better breed management.</p>
                         </div>
+                        
                     </div>
                 </section>
                 <section>
