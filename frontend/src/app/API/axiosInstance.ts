f8522026c88704029eedf90b65a56fd4852c4f2a
Sam njoro <samnjorokibandi@gmail.com>
2025-08-09 21:26:22 +0300

    setting axios interceptors

--- frontend/src/app/API/axiosInstance.ts
+++ frontend/src/app/API/axiosInstance.ts
@@ -4,6 +4,34 @@ const axiosInstance = axios.create({
     baseURL: "http://localhost:8000/", //process.env.BACKEND_URL,
 });
 
+axiosInstance.interceptors.request.use(
+    (config) => {
+        config.withCredentials = true;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+
+);
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) =>{
+        if(error.response && error.response.status===401){
+            try{
+               await axiosInstance.post("/auth/refresh", {}, {withCredentials: true});
+
+               return axiosInstance(error.config);
+            }
+            catch(refreshError){
+               window.location.href="/auth/login"
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
+
 axiosInstance.defaults.withCredentials=true
 
 export default axiosInstance;
