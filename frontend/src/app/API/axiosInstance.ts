bb4de21a42a8755cb9330db5770c58340d82ac6c
Sam njoro <samnjorokibandi@gmail.com>
2025-08-09 21:40:33 +0300

    dashboard updates

--- frontend/src/app/API/axiosInstance.ts
+++ frontend/src/app/API/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/", //process.env.BACKEND_URL,
+    baseURL: process.env.NEXT_RENDER_URL,//"http://localhost:8000/", //process.env.BACKEND_URL,
+    withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
@@ -17,20 +18,25 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
     (response) => response,
     async (error) =>{
+        const originalRequest = error.config;
+
+        if(originalRequest.url.includes('/auth/refresh')){
+            window.location.href="/auth/login"
+            return Promise.reject(error);
+        }
         if(error.response && error.response.status===401){
             try{
                await axiosInstance.post("/auth/refresh", {}, {withCredentials: true});
-
-               return axiosInstance(error.config);
+               return axiosInstance(originalRequest);
             }
             catch(refreshError){
                window.location.href="/auth/login"
+               return Promise.reject(refreshError);
             }
         }
         return Promise.reject(error);
     }
 );
-
 
 axiosInstance.defaults.withCredentials=true
 
