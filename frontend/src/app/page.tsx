8dd2a0883a26e0a562386aa402de6bda7782bdd2
Sam njoro <samnjorokibandi@gmail.com>
2025-08-17 01:39:38 +0300

    update for github workflow and pages layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,7 @@
-// import Register from './auth/register/page';
 import Homepage from './page/page';
 export default function Home() {
   return (
     <div>
-      {/* < Register /> */}
       < Homepage />
     </div>
   );
