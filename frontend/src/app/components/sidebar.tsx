c7dfe9f07a237ceecfc3384fa33fe088642889c2
Sam njoro <samnjorokibandi@gmail.com>
2025-08-18 13:22:17 +0300

    styling  sidebar

--- frontend/src/app/components/sidebar.tsx
+++ frontend/src/app/components/sidebar.tsx
@@ -1,26 +1,31 @@
-
-
 "use client"
+import { useState } from "react";
 
+export default function Sidebar() {
 
-export default function Sidebar (){
-    return(
-        <div className="">
-            <div className="">
-                <div className="">
-                    <button>Dashboard</button>
-                </div>
-                <div>
-                
-                    <button>Farm Inputs</button>
-                    <button>AI Insights</button>
-                    <button>Farm Analytic</button>
-                    <button>Alerts</button>
-                    <button>Farmer Profile </button>
-                </div>
-
-            </div>
-
+  return (
+      <div className="fixed md:static top-0 left-0 h-screen md:h-auto w-70 bg-green-100 shadow-md p-4 space-y-6 mx-5 transform transition-transform duration-300 ">
+        <div>
+          <h2 className="text-green-700 font-semibold mb-2 border-b border-green-300">Dashboard</h2>
+          <div className="flex flex-col space-y-2">
+            <button className="text-left px-3 py-2 text-green-400 rounded-md hover:bg-green-200">Overview</button>
+            <button className="text-left px-3 py-2 text-green-400 rounded-md hover:bg-green-200">Notifications</button>
+          </div>
         </div>
-    )
+
+        <div>
+          <h2 className="text-green-700 font-semibold mb-2 border-b border-green-300">Product</h2>
+          <div className="flex flex-col space-y-2">
+            <button className="text-left px-3 py-2 text-green-400 rounded-md hover:bg-green-200">Farm Inputs</button>
+            <button className="text-left px-3 py-2 text-green-400 rounded-md hover:bg-green-200">AI Insights</button>
+            <button className="text-left px-3 py-2 text-green-400 rounded-md hover:bg-green-200">Farm Analytics</button>
+            <button className="text-left px-3 py-2 text-green-400 rounded-md hover:bg-green-200">Alerts</button>
+            <button className="text-left px-3 py-2 text-green-400 rounded-md hover:bg-green-200">Farmer Profile</button>
+          </div>
+        </div>
+        <div className="mt-35 border-t border-green-300">
+            <button className="text-green-400  px-3 py-2  rounded-md hover:bg-green-300 transition">Contact Support</button>
+        </div>
+      </div>
+  );
 }
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,11 +2,15 @@
 
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Sidebar from '../components/sidebar';
 
 export default function Dashboard(){
     return(
-        <div>
+        <div className="bg-green-50 flex flex-col min-h-screen">
             <Header />
+            <main className="flex-grow">
+                <Sidebar />
+            </main>
             <Footer />
         </div>
     )
