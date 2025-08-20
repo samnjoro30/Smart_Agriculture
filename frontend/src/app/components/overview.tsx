67612d129d0ca67d51594abb678309fb982ff049
Sam njoro <samnjorokibandi@gmail.com>
2025-08-20 09:56:39 +0300

    overview structure

---
+++ frontend/src/app/components/overview.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import {useState, useEffect } from 'react';
+import axiosInstance from '../API/axiosInstance';
+
+interface User {
+    farm: string,
+    name: string,
+    email: string,
+    PhoneNumber: string
+}
+
+export default function Overview(){
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() =>{
+        const fetchUserDetails = async () =>{
+
+            try{
+                const res = await axiosInstance.get("/user/userDetails")
+                setUser(res.data.message);
+
+            }catch(err){
+                const error = err instanceof Error ? err : new Error(String(err));
+                console.error("Error occurred fetching details", error)
+            }
+
+        }
+        fetchUserDetails();
+    }, []);
+    return(
+        <div>
+            <div>
+                {user ? (
+                    <ul>
+                        <li><strong>Name:</strong> {user.name}</li>
+                        <li><strong>Email:</strong> {user.email}</li>
+                    </ul>
+
+                ) : (
+                    <p></p>
+                )}
+
+            </div>
+            <div>
+                {user ? (
+                    <ul>
+                        <li><strong>Farm:</strong> {user.farm}</li>
+                        <li><strong>Phone:</strong> {user.PhoneNumber}</li>
+                    </ul>
+
+                ): (
+                    <p></p>
+                )}
+            </div>
+            <div>
+                <div>
+                    <p>Updates</p>
+
+                </div>
+                <div>
+                    <p>Insights</p>
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
