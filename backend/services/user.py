465fff0e41f85f9f2fbd684b90a3b7768281099d
Sam njoro <samnjorokibandi@gmail.com>
2025-08-31 01:27:31 +0300

    backend users profile functions

--- backend/services/user.py
+++ backend/services/user.py
@@ -10,3 +10,28 @@ async def get_userProfile(db: AsyncSession, email: str):
     results = await db.execute(query, {"email": email})
     return results.mappings().first()
 
+async def check_user_by_email(db: AsyncSession, email: str):
+   query = text("""
+       SELECT email FROM users WHERE email = :email
+   """)
+   results = await db.execute(query, {"email": email})
+   row = results.fetchone()
+   if row:
+      return {
+         "email": email
+      }
+
+async def UpdateEmail(db: AsyncSession, email: str):
+   query = text("""
+      UPDATE email FROM users WHERE email = :email
+   """)
+   await db.execute(query, {"email": email})
+   await db.commit()
+
+async def UpdatePhoneNumber(db: AsyncSession, phonenumber: str):
+   query = text("""
+      UPDATE phonenumber FROM users WHERE phonenumber = :phonenumber
+   """)
+   await db.execute(query, {"phonenumber": phonenumber})
+   await db.commit()
+
