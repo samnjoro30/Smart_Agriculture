9e14c0d0bd275a27ad9cf68ef41175466f6526ff
Sam njoro <samnjorokibandi@gmail.com>
2025-09-12 20:05:21 +0300

    backend cow ddb table

--- backend/model/farming.py
+++ backend/model/farming.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel, EmailStr, constr
 
 class Register_cow(BaseModel):
-    cowname = str
+    name: str
+    age: int 
+    lastBirth: str
+    lastMate: str
+    calf: int
---
+++ backend/model/farming_table.py
@@ -0,0 +1,13 @@
+from db.postgre_db import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+
+
+class FarmingInput(Base):
+    __tablename__ = "cows"
+
+    id = Column(Integer, primary_key=True, index=True)
+    cowname = Column(String(255))
+    cowage = Column(Integer(200))
+    cowlastBirth = Column(DateTime, nullable=True)
+    lastMate = Column(DateTime, nullable=True)
+    calf = Column(Integer)
