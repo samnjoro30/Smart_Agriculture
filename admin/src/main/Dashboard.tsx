6492aa4f4fa5941f6b13d1846eb09c6836a95485
Sam njoro <samnjorokibandi@gmail.com>
2025-12-30 22:16:47 +0300

    footer updates

--- admin/src/main/Dashboard.tsx
+++ admin/src/main/Dashboard.tsx
@@ -1,12 +1,13 @@
 import Header from "../components/header";
 import Sidebar from "../components/sideview";
+import Footer from "../components/footer";
 import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     return( 
-        <div>
+        <div  className='min-h-screen flex flex-col'>
             <Header />
-            <div className="pt-16 flex">
+            <div className="pt-16 flex flex-1 pb-10">
             <aside className="relative left-0 mt-5">
                 <Sidebar />
             </aside>
@@ -15,7 +16,9 @@ const Dashboard = () => {
                 <Outlet />
                 <h2 className="text-xl font-semibold text-center mt-6">Welcome to the Smart Farm Admin Dashboard</h2>
             </main>
+            
             </div>
+            < Footer />
         </div>
 
     )
