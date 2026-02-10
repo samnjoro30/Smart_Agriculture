63f136cbcdf44abaf7f95742d247ad770c6043f4
Sam njoro <samnjorokibandi@gmail.com>
2026-02-10 15:23:00 +0300

    configuring logger structLog

---
+++ backend/config/logger.py
@@ -0,0 +1,34 @@
+import logging
+import sys
+import structlog
+from config.settings import settings
+
+
+def setup_logging():
+    timestamper = structlog.processors.TimeStamper(fmt="iso")
+
+    shared_processors = [
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        timestamper,
+    ]
+
+    # dev → pretty logs
+    if settings.ENV == "dev":
+        renderer = structlog.dev.ConsoleRenderer()
+    else:
+        # prod → JSON logs
+        renderer = structlog.processors.JSONRenderer()
+
+    structlog.configure(
+        processors=shared_processors + [renderer],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        logger_factory=structlog.PrintLoggerFactory(file=sys.stdout),
+        cache_logger_on_first_use=True,
+    )
+
+
+def get_logger(name: str):
+    return structlog.get_logger(name)
