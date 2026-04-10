9ecb3dc119234d767ad9b9e29c1a877552b6953b
Sam njoro <samnjorokibandi@gmail.com>
2026-04-10 22:27:01 +0300

    fixed config render ssl issue on ca cert

--- backend/worker/celery_config.py
+++ backend/worker/celery_config.py
@@ -1,6 +1,11 @@
+import ssl
+import os
+import certifi
 from config.setting import get_settings
 
 settings = get_settings()
+
+ISPRODUCTION = os.getenv("RENDER", "false").lower() == "true"
 
 CELERY_CONFIG = {
     "broker_url": settings.REDIS_URL_BROKER,
@@ -21,3 +26,17 @@ CELERY_CONFIG = {
     "worker_prefetch_multiplier": 1,
     "task_reject_on_worker_lost": True,
 }
+
+if ISPRODUCTION:
+    # Use strict SSL for Render
+    ssl_conf = {
+        "ssl_cert_reqs": ssl.CERT_REQUIRED,
+        "ca_certs": certifi.where(),
+    }
+    CELERY_CONFIG["broker_use_ssl"] = ssl_conf
+    CELERY_CONFIG["redis_backend_use_ssl"] = ssl_conf
+else:
+    # Handle local dev logic
+    if settings.REDIS_URL_BROKER.startswith("rediss"):
+        CELERY_CONFIG["broker_use_ssl"] = {"ssl_cert_reqs": ssl.CERT_NONE}
+        CELERY_CONFIG["redis_backend_use_ssl"] = {"ssl_cert_reqs": ssl.CERT_NONE}
