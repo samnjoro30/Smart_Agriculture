4f81abc318142d94a4bee6511519f02afc656777
Sam njoro <samnjorokibandi@gmail.com>
2026-04-22 23:05:04 +0300

    websocket connection setup

---
+++ backend/hooks/router.py
@@ -0,0 +1,23 @@
+from fastapi import WebSocket
+from .services import WebSocketManager
+from config.security import get_current_user
+from fastapi import APIRouter
+
+app = APIRouter()
+
+@app.websocket("/ws/{user_id}")
+async def websocket_endpoint(websocket: WebSocket, user_id: str):
+    token  = websocket.query_params.get("token")
+    user_id = await get_current_user(token=token)
+
+    if not user_id:
+        await websocket.close(code=1008)
+        return
+    
+    await manager.connect(websocket, user_id)
+
+    try:
+        while True:
+            await websocket.receive_text()
+    except Exception as e:
+        manager.disconnect(websocket, user_id)
