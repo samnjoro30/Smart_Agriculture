c682dd24e31340886cf81ff1e8a55dd374a6e58a
Sam njoro <samnjorokibandi@gmail.com>
2026-06-02 23:05:21 +0300

    lidespan file redis connection and server ping, db warm boot

--- backend/config/lifespan.py
+++ backend/config/lifespan.py
@@ -1,9 +1,20 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from .logger import get_logger
-from .redis import init_redis, close_redis
+import asyncio
+import httpx
+
+from config.audit.logger import get_logger
+from config.redis.client import init_redis, close_redis
+from config.setting import get_settings
+
+from hooks.tasks import cleanup_dead_connections
+from hooks.call import manager
 
 logger = get_logger("LIFESPAN")
+
+limiter = None
+
+settings = get_settings()
 
 
 @asynccontextmanager
@@ -12,6 +23,33 @@ async def Lifespan(app: FastAPI):
 
     await init_redis()
     logger.info("Redis connection established")
+
+    asyncio.create_task(manager.send_ping())
+    asyncio.create_task(cleanup_dead_connections(manager))
+
+    async def warm_db():
+        async with engine.begin() as conn:
+            await conn.execute(text("SELECT 1"))
+
+    async def keep_alive():
+        async with httpx.AsyncClient(timeout=5.0) as client:
+
+            while True:
+                try:
+                    await client.get(
+                        "https://smart-agriculture-21dt.onrender.com/ping", timeout=5
+                    )
+                    print("Self-ping successful")
+                except Exception as e:
+                    print("Self-ping failed:", e)
+
+                await asyncio.sleep(600)
+
+    asyncio.create_task(keep_alive())
+
+    async def shutdown():
+        await close_redis()
+
 
     yield
     logger.info("Redis server Down")
