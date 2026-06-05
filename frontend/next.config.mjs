0918a55c25fdda88fbae7aadc5b058fb6f40fbca
Sam njoro <samnjorokibandi@gmail.com>
2026-06-05 23:27:48 +0300

    packages updates

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -13,6 +13,7 @@ export default nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
+ // disable: process.env.NODE_ENV === "development",
 })(nextConfig);
 
 // import type { NextConfig } from "next";
